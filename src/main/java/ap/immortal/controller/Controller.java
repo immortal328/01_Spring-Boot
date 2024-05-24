ca7324920560d51fff81d65685e6c253d8af2b6c
Amar H. Pujari. <47078596+immortal328@users.noreply.github.com>
2024-05-25 03:05:29 +0530

    accessing values from property file and yml config

--- src/main/java/ap/immortal/controller/Controller.java
+++ src/main/java/ap/immortal/controller/Controller.java
@@ -1,5 +1,6 @@
 package ap.immortal.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,16 +8,28 @@ import ap.immortal.model.Student;
 
 @RestController
 public class Controller {
-	
+	@Value("${props.name.firstName}")
+	private String firstName;
+
+	@Value("${props.name.lastName}")
+	private String lastName;
+
 	@GetMapping("/")
 	public String sayHello() {
 		return "Hello User...";
 	}
-	
+
 	@GetMapping("/getStudent")
 	public Student getStudent() {
-		return new Student(28,12,"Test","Test","test.test@gmail.com");
-		
+		return new Student(28, 12, "Test", "Test", "test.test@gmail.com");
+
+	}
+
+	@GetMapping("/getStudentProps")
+	public Student getStudentProps() {
+		return new Student(28, 12, firstName, lastName,
+				String.format("%s.%s@gmail.com", firstName, lastName).toLowerCase());
+
 	}
 
 }
